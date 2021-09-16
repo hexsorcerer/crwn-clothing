680cebf12a6819fb7f65f8ef744f1d7acc16eb6e
hexsorcerer <hexsorcerer03e9@gmail.com>
2021-09-15 20:52:45 -0400

    feat: add images to homepage
    Previously, the base items on the homepage were just text in empty boxes. Now, background images are
    loaded into each item. This involved breaking out a lot of inline html into separate components with
    their own styles.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import DDirectory from '../../components/directory/directory.component'
+
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+    <Directory></Directory>
+);
+
+export default HomePage;
